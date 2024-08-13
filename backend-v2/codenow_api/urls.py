b772014964b408cda9e91cb3fe4813661f0fbf59
Abdullah Shahid <nxabdullah1@gmail.com>
2024-08-13 19:08:45 -0400

    [#22] Add 150 problems with management script

--- backend-v2/codenow_api/urls.py
+++ backend-v2/codenow_api/urls.py
@@ -4,7 +4,7 @@ from drf_spectacular.views import SpectacularSwaggerView
 
 urlpatterns = [
     path('account/', include('accounts.urls')),
-    path('problems/', include('problems.urls')),
+    path('', include('problems.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs')
 ]
--- backend-v2/problems/views.py
+++ backend-v2/problems/views.py
@@ -1,24 +1,46 @@
 from rest_framework import generics
 from .models import Problem
-from .serializers import ProblemSerializer
-from rest_framework import generics
+from .serializers import ProblemListSerializer, ProblemDetailSerializer, AttemptSerializer
+from rest_framework import generics, permissions, status, response
 
-class ProblemListCreate(generics.ListCreateAPIView):
+class ProblemList(generics.ListAPIView):
     """
-    API view to retrieve a list of problems or create a new problem.
-
-    GET:
-    Return a list of all existing problems. Supports filtering, searching, and ordering 
-    based on specific fields if configured in the serializer or view.
-
-    POST:
-    Create a new problem instance. The request must include all required fields 
-    specified in the ProblemSerializer. Returns the created problem data if successful.
-
-    Attributes:
-        queryset: A queryset that retrieves all Problem instances from the database.
-        serializer_class: The serializer class that handles the serialization and 
-        deserialization of Problem instances.
+    API view to retrieve a list of problems.
     """
     queryset = Problem.objects.all()
-    serializer_class = ProblemSerializer
+    serializer_class = ProblemListSerializer
+    pagination_class = None
+
+    def get_serializer_context(self):
+        """
+        Add the request context to the serializer context.
+        """
+        context = super().get_serializer_context()
+        include_param = self.request.query_params.get('include', '')
+        context['include_lastAttempt'] = 'lastAttempt' in include_param.split(',')
+        return context
+
+class AttemptListCreate(generics.RetrieveAPIView, generics.CreateAPIView):
+    """
+    API view to retrieve a problem and its list of attempts or create a new attempt for a specific problem.
+    """
+    queryset = Problem.objects.all()
+    serializer_class = ProblemDetailSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+    pagination_class = None
+
+    def post(self, request, *args, **kwargs):
+        """
+        Handle POST requests to create a new attempt for the specific problem.
+        """
+        problem = self.get_object()
+
+        attempt_data = request.data.copy()
+        attempt_data['user'] = request.user.id
+        attempt_data['problem'] = problem.id
+
+        serializedAttempt = AttemptSerializer(data=attempt_data)
+        if serializedAttempt.is_valid():
+            serializedAttempt.save(problem=problem, user=request.user)
+            return response.Response(serializedAttempt.data, status=status.HTTP_201_CREATED)
+        return response.Response(serializedAttempt.errors, status=status.HTTP_400_BAD_REQUEST)
