b2abdf7f8afad609bd4612180525f291a4eeb7cb
Abdullah Shahid <90593598+nxabdullah@users.noreply.github.com>
2024-08-13 19:10:01 -0400

    [#22] Add 150 problems with management script (#23)

--- backend-v2/problems/models.py
+++ backend-v2/problems/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth import get_user_model
 
 class Problem(models.Model):
@@ -34,28 +35,73 @@ class Problem(models.Model):
         MATH_GEOM = 'math-geom', 'Math and Geometry'
         BIT_MANIP = 'bit-manip', 'Bit Manipulation'
 
+    id = models.CharField(max_length=100, primary_key=True, verbose_name="Problem ID")
+
     name = models.CharField(max_length=100, unique=True)
 
-    leetcode_url = models.URLField(unique=True)
+    lc_id = models.CharField(max_length=100, unique=True)
 
     difficulty = models.CharField(max_length=7, choices=Difficulty.choices)
 
     category = models.CharField(max_length=100, choices=Category.choices)
 
+    def get_last_attempt(self, user):
+        if user.is_anonymous:
+            return None
+        return Attempt.objects.filter(user=user, problem=self).order_by('-timestamp').first()
+
+    def get_attempts(self, user):
+        if user.is_anonymous:
+            return None
+        return Attempt.objects.filter(user=user, problem=self).order_by('-timestamp').all()
+
     def __str__(self):
-        return f'{self.get_difficulty_display()} - {self.leetcode_url}'
+        return f'Problem "{self.name}" - Category: {self.get_category_display()}, Difficulty: {self.get_difficulty_display()}'
 
-
+# TODO: Consider adding efficiency, notes and solution section. 
 class Attempt(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
-    perceived_difficulty = models.CharField(max_length=7, choices=Problem.Difficulty.choices)
-    time_taken = models.PositiveIntegerField(help_text="Time taken in minutes")
-    date_time = models.DateTimeField(auto_now_add=True)
-    completed = models.BooleanField(default=False)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name="User")
+    problem = models.ForeignKey('Problem', on_delete=models.CASCADE, verbose_name="Problem")
+    timestamp = models.DateTimeField(auto_now_add=True, verbose_name="Timestamp")
+    duration = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(1440)],
+        verbose_name="Duration (minutes)",
+        null=True,  # Allow null to indicate in progress
+        blank=True
+    )
+    score = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(10)],
+        verbose_name="Score",
+        null=True,  # Allow null to indicate in progress
+        blank=True
+    )
+    num_attempts = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
+        verbose_name="Number of attempts until completed",
+        null=True,  # Allow null to indicate in progress
+        blank=True
+    )
 
     class Meta:
-        unique_together = ('user', 'problem', 'date_time')
+        unique_together = ('user', 'problem', 'timestamp')
+        verbose_name = "Problem Attempt"
+        verbose_name_plural = "Problem Attempts"
 
     def __str__(self):
-        return f'{self.user.username} - {self.problem.leetcode_url} - {self.date_time}'
+        return f'Attempt by {self.user.username} on {self.problem.name} - Started at {self.timestamp.strftime("%Y-%m-%d %H:%M:%S")} with score: {self.score if self.score is not None else "N/A"}'
+
+    @property
+    def is_in_progress(self):
+        """
+        Returns True if the attempt is in progress, meaning score, duration, 
+        and num_attempts are all None (indicating that the attempt has not been completed).
+        """
+        return self.score is None and self.duration is None and self.num_attempts is None
+
+    @property
+    def has_abandoned(self):
+        """
+        Returns True if the user has abandoned the problem, 
+        which is defined as having a score of 0.
+        """
+        return self.score == 0
--- backend-v2/problems/urls.py
+++ backend-v2/problems/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ProblemListCreate.as_view(), name='problem-list-create'),
+    path('problems/', views.ProblemList.as_view(), name='problem-list-create'),
+    path('problems/<int:pk>/', views.AttemptListCreate.as_view(), name='problem-attempt-list-create'),
 ]
